b8191803a1fa40eebf293bcfb609238c7be2625a
DESKTOP-9SRLHKE\Aussie <layan.wijesekera@live.com>
2020-06-20 20:30:13 +1000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+    >
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-column">
+    <v-btn @click="openProject">
+      Testing
+    </v-btn>
+    <v-btn @click="openProject2">
+      Testing2
+    </v-btn>
+
+    <div style="width: 20%">
+      <v-img src="https://media.gettyimages.com/photos/big-lion-showing-who-is-the-king-picture-id97992631?s=612x612" height="200" width="400" contain>
+      </v-img>
+      <v-img src="@/assets/lion1.jpg" height="400" width="400" contain>
+      </v-img>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+
+  methods: {
+    openProject() {
+      console.log("I was clicked !!!");
+    },
+    openProject2() {
+      console.log("I was clicked yo!!!");}
+
+  }
+}
+</script>
